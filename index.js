4e4ccc20684bc95c8db680b7d85860a2d735c9d0
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2020-08-17 10:18:39 +0900

    connect mongoDB

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://Benny:jh0210@boilerplate.kvdus.mongodb.net/test?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
